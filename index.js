9d83add9968f79de6e4c65bf04bc7d59ca89eb8e
Muhammad Zeeshan <79660812+ShaniDevelp@users.noreply.github.com>
2024-03-11 21:47:55 +0500

    name change

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use('/categories', categoryRouter);
 app.use('/cart', cartRouter);
 app.use('/orders', orderRouter);
 app.use('/addresses', addressRouter);
-app.get('*', (req, res) =>
+app.use('*', (req, res) =>
   res.sendFile(path.resolve('build',  'index.html'))
 );
 
@@ -64,16 +64,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.get('/', (req, res) => {
   res.send({ status: 'running' })
