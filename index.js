acdb8042ba32d3699eba16eead3490d236bff16b
Muhammad Zeeshan <79660812+ShaniDevelp@users.noreply.github.com>
2024-09-26 12:39:01 +0500

    new chnages

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function main() {
 
 app.use(cors({
   exposedHeaders: ['X-Total-Count'],
-  origin: ["https://e-commerce-cdi9.vercel.app"],
+  origin: ["http://localhost:3000"],
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   credentials: true
 }));
